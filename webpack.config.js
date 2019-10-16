9c0f056dd7b3d278a56ef45ff82a8004ecafdd3a
Daniil <39dv@mail.ru>
2019-10-16 22:01:06 +0300

    Update publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = '/Gh0st0fwAr.github.io/';
+  const publicPath = '/dist/';
 
   const pcss = {
     test: /\.(p|post|)css$/,
