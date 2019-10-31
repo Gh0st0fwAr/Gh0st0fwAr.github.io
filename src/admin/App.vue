10cb8f6d273937563ea46f11fcbe09b0e51f63ce
Daniil <39dv@mail.ru>
2019-10-31 18:31:21 +0300

    Реализован хедер и навигация админки

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,3 +1,23 @@
 <template lang="pug">
-  h1 Welcome to the Vue App
+  section.reviews
+    header.header-admin
+      .container-admin.container-reviews
+        .header-admin__person
+          .person__img
+            img(src="../images/content/myface.jpg").person__pic
+          .person__title Даниил Вирин
+        .header-admin__desc Панель администрирования
+        .header-admin__leave 
+          a.leave__link Выйти
+    nav.breadcrumbs 
+      ul.breadcrumbs__list
+        li.breadcrumbs__item
+          a.breadcrumbs__link Обо мне
+        li.breadcrumbs__item
+          a.breadcrumbs__link Работы
+        li.breadcrumbs__item
+          a.breadcrumbs__link Отзывы
 </template>
+
+<style src="./../styles/main.pcss">
+</style>
