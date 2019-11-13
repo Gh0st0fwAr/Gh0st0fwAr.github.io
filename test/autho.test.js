a7e64d0598e210cafe87b185122717815b07feb2
Daniil <39dv@mail.ru>
2019-11-13 09:19:55 +0300

    Прикручен тест

--- test/autho.test.js
+++ test/autho.test.js
@@ -1,2 +1,26 @@
-const Autho = require('../src/admin');
-const autho = new Autho();
+const fs = require('fs');
+const webdriverio = require('webdriverio');
+const options = require('../wdio.conf');
+const client = webdriverio.multiremote(options);
+
+jest.setTimeout(30000);
+
+beforeAll(() => {
+    return client.init().url('http://localhost:8080/admin');
+});
+
+test('на странице есть кнопка "Авторизоваться"', () => {
+   return client
+       .isExisting('.login__button')
+       .then(browsers => {
+           for (const browserName in browsers) {
+               expect(browsers[browserName]).toBe(true);
+           }
+       })
+       .screenshot()
+       .then(browsers => {
+           for (const browserName in browsers) {
+               fs.writeFileSync(`./screenshots/overlay_${browserName}_has_open.png`, browsers[browserName].value, 'base64');
+           }
+       });
+});
